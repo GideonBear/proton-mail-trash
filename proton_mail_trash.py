6cedc60acac9e6ec1280b6e4ffa4f838b59e57a3
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-04-23 14:55:56 +0200

    Initial commit

---
+++ proton_mail_trash.py
@@ -0,0 +1,10 @@
+import webbrowser
+from datetime import date, timedelta, datetime
+
+today = date.today()
+interval = timedelta(days=30)
+
+end = today - interval
+end = datetime.combine(end, datetime.min.time())
+url = f"https://mail.proton.me/u/0/trash#end={int(end.timestamp())}"
+webbrowser.open(url)
