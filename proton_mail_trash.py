f4058f77505b6ba4ee582d73d8e4183c6e263af8
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-05-05 08:56:33 +0200

    Add drive trash

--- proton_mail_trash.py
+++ proton_mail_trash.py
@@ -8,3 +8,6 @@ end = today - interval
 end = datetime.combine(end, datetime.min.time())
 url = f"https://mail.proton.me/u/0/trash#end={int(end.timestamp())}"
 webbrowser.open(url)
+
+url = "https://drive.proton.me/u/0/trash"
+webbrowser.open(url)
