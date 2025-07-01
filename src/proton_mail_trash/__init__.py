a526ab42b0827639ee13fb2077debbb454fd1f74
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-07-01 14:08:57 +0200

    Use Hydroxide and IMAP to automate trashing

--- src/proton_mail_trash/__init__.py
+++ src/proton_mail_trash/__init__.py
@@ -1,18 +1,63 @@
 from __future__ import annotations
 
-import webbrowser
-from datetime import date, datetime, timedelta
+import sys
+from datetime import date, timedelta
+from email.parser import HeaderParser
+from getpass import getpass
+from imaplib import IMAP4
+
+from proton_mail_trash import hydroxide
+
+
+PORT = 1143
 
 
 def main() -> None:
-    today = date.today()  # noqa: DTZ011 only using naive datetimes
-    interval = timedelta(days=30)
+    user = input("E-mail: ")
+    with hydroxide.run(user):
+        password = getpass("Password: ")
 
-    end = today - interval
-    end = datetime.combine(end, datetime.min.time())
-    url = f"https://mail.proton.me/u/0/trash#end={int(end.timestamp())}"
-    webbrowser.open(url)
+        today = date.today()  # noqa: DTZ011 only using naive datetimes
+        interval = timedelta(days=30)
 
-    # Manually select trash from the last 30 days
-    url = "https://drive.proton.me/u/0/trash"
-    webbrowser.open(url)
+        end = today - interval
+        criteria = f"(BEFORE {end.strftime('%d-%b-%Y')})"
+
+        box = IMAP4("localhost", PORT)
+        box.login(user, password)
+        box.select("Trash")
+        _typ, data = box.search(None, criteria)
+        uids = data[0].split()
+
+        mailparser = HeaderParser()
+        to_delete = []
+        print("### The following messages will be permanently deleted:")
+        for uid in uids:
+            _resp, data = box.uid("fetch", uid, "(BODY[HEADER])")
+            msg = mailparser.parsestr(data[0][1].decode())
+            info = f"{msg['From']}, {msg['Date']}, {msg['Subject']}"
+            info = info.replace("\n", "").replace("\r", "")
+            print(info)
+            to_delete.append(uid)
+
+        if (
+            input(
+                "### The preceding messages will be permanently deleted. "
+                "Continue? (y/n) "
+            )
+            != "y"
+        ):
+            print("Abort.")
+            sys.exit(1)
+
+        for uid in to_delete:
+            print(uid.decode())
+            box.store(uid, "+FLAGS", "\\Deleted")
+
+        print("Expunging...")
+        box.expunge()
+        print("Closing...")
+        box.close()
+        print("Logging out...")
+        box.logout()
+        print("Stopping Hydroxide...")
